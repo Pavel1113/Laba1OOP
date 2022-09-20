27d2aa2c2a6e917dcaf65d5dd3a50a09eb17616b
Pavel1113 <bichpasha@yandex.ru>
2022-09-20 08:48:06 +0300

    Initial commit

---
+++ app/src/main/java/pavelbich/fit/bstu/laba1oop/MainActivity.java
@@ -0,0 +1,14 @@
+package pavelbich.fit.bstu.laba1oop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
